833b4054a3abf2c38925bdc5e3b662afa0466efa
Bartosz Gajda <bg@bartoszgajda.com>
2020-01-10 14:00:38 +0000

    Val, Vars and Types.

---
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,20 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+  val x: Int = 42
+  println(x)
+
+  val aString: String = "string"
+  val anotherString = "another string"
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val asInt: Int = x
+  val aShort: Short = 1234
+  val aLong: Long = 12340000L
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 3.0
+
+  var aVariable: Int = 4
+  aVariable = 5
+}
