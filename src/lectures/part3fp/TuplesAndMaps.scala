bcc3d03498e2501ee2ec86ac5338fd7608d9f52f
Bartosz Gajda <bg@bartoszgajda.com>
2020-01-18 16:50:16 +0000

    Tuples and Maps exercises.

--- src/lectures/part3fp/TuplesAndMaps.scala
+++ src/lectures/part3fp/TuplesAndMaps.scala
@@ -27,4 +27,41 @@ object TuplesAndMaps extends App {
   println(phonebook.toList)
   val names = List("Bob", "James", "Angela")
   println(names.groupBy(name => name.charAt(0)))
+
+  def add(network: Map[String, Set[String]], person: String): Map[String, Set[String]] = {
+    network + (person -> Set())
+  }
+  def friend(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
+    val friendsA = network(a)
+    val friendsB = network(b)
+
+    network + (a -> (friendsA + b)) + (b -> (friendsB + a))
+  }
+  def unfriend(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
+    val friendsA = network(a)
+    val friendsB = network(b)
+
+    network + (a -> (friendsA - b)) + (b -> (friendsB - a))
+  }
+  def remove(network: Map[String, Set[String]], person: String): Map[String, Set[String]] = {
+    def removeAux(friends: Set[String], networkAcc: Map[String, Set[String]]): Map[String, Set[String]] = {
+      if (friends.isEmpty) networkAcc
+      else removeAux(friends.tail, unfriend(networkAcc, person, friends.head))
+    }
+    val unfriended = removeAux(network(person), network)
+    unfriended - person
+  }
+
+  def nFriends(network: Map[String, Seq[String]], person: String): Int = {
+    if (!network.contains(person)) 0
+    else network(person).size
+  }
+
+  def mostFriends(network: Map[String, Seq[String]]): String = {
+    network.maxBy(pair => pair._2.size)._1
+  }
+
+  def nPeopleWithNoFriends(network: Map[String, Seq[String]]): Int = {
+    network.count(_._2.isEmpty)
+  }
 }
