8e2120c81a03e5a3b9beeebab2dac7d555758659
Bartosz Gajda <bg@bartoszgajda.com>
2020-01-18 15:17:57 +0000

    Map, FlatMap and Filter.

---
+++ src/lectures/part3fp/MapFlatmapFilterFor.scala
@@ -0,0 +1,43 @@
+package lectures.part3fp
+
+object MapFlatmapFilterFor extends App {
+  val list = List(1, 2, 3)
+  println(list.head)
+  println(list.tail)
+
+  // map
+  println(list.map(_ + 1))
+  println(list.map(_ + " is a number"))
+
+  // filter
+  println(list.filter(_ % 2 == 0))
+
+  // flatMap
+  val toPair = (x: Int) => List(x, x + 1)
+  println(list.flatMap(toPair))
+
+  // iterating with map and flatMap
+  val numbers = List(1, 2, 3, 4)
+  val chars = List('a', 'b', 'c', 'd')
+  val colors = List("black", "white")
+  println(numbers.flatMap((number: Int) => chars.map((char: Char) => char + "" + number)))
+
+  list.foreach(println)
+
+  // for-comprehensions
+  val forCombinations = for {
+    n <- numbers if n % 2 == 0 // guard
+    c <- chars
+    color <- colors
+  } yield "" + c + n + "-" + color
+  println(forCombinations)
+
+  for {
+    n <- numbers
+  } println(n)
+
+  // syntax overload
+  list.map { x =>
+    x * 2
+  }
+}
