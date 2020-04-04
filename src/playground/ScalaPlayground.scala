22174925f7174e5df316491973dae63db54cfe79
Bartosz Gajda <bg@bartoszgajda.com>
2020-04-04 02:15:56 +0100

    Cleanup playground file.

--- src/playground/ScalaPlayground.scala
+++ src/playground/ScalaPlayground.scala
@@ -1,18 +1,18 @@
 package playground
 
 object ScalaPlayground extends App {
-  val car = new Car("ford")
-  val bus = new Bus("fiat")
-
-  val vehicleList: VehicleList[Vehicle] = new VehicleList[Car](new Car("bmw"))
-  val carList: CarList[Car] = new CarList[Vehicle](new Vehicle("boat"))
+//  val car = new Car("ford")
+//  val bus = new Bus("fiat")
+//
+//  val vehicleList: VehicleList[Vehicle] = new VehicleList[Car](new Car("bmw"))
+//  val carList: CarList[Car] = new CarList[Vehicle](new Vehicle("boat"))
 }
 
-class Vehicle(name: String)
-
-class Car(name: String) extends Vehicle(name)
-class Bus(name: String) extends Vehicle(name)
-
-class VehicleList[+T](val vehicle: T)
-class CarList[-T](val car: T)
-class List[T](val t: T)
+//class Vehicle(name: String)
+//
+//class Car(name: String) extends Vehicle(name)
+//class Bus(name: String) extends Vehicle(name)
+//
+//class VehicleList[+T](val vehicle: T)
+//class CarList[-T](val car: T)
+//class List[T](val t: T)
